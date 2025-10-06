a6bbf9deea9135f3e2db5f277c35cff5b2d7b1a9
Furkan Kuluncakli <furkankuluncakli@gmail.com>
2025-10-06 21:23:46 +0300

    Set up Express server with dotenv configuration and nodemon for development

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,13 @@
 import express from "express";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+
+app.get("/test",(res,req)=>{
+    res.status(200).send("AMINAGOYUM")
+})
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is Running on port : ${process.env.PORT}`);
+});
