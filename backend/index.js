cabce2b9f5fa969dc09e71c8d44eb2bf39267b7d
Furkan Kuluncakli <furkankuluncakli@gmail.com>
2025-10-07 20:39:00 +0300

    Refactor backend routes: add user, post, and comment routers

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
+import userRouter from "./routes/users.route.js";
+import postRouter from "./routes/posts.route.js";
+import commentRouter from "./routes/comments.route.js";
 
 const app = express();
 dotenv.config();
 
-app.get("/test",(res,req)=>{
-    res.status(200).send("AMINAGOYUM")
-})
+app.use("/users", userRouter);
+app.use("/comments", commentRouter);
+app.use("/posts", postRouter);
+
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Server is Running on port : ${process.env.PORT}`);
